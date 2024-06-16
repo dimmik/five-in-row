aba5edc43e3a46d2af0f7d1d5f98b01450986f41
dimmik <ad@dimmik.org>
2024-06-16 13:07:37 +0200

    Calculate winner

--- FiveInRowDomain/FiveInRowGame.cs
+++ FiveInRowDomain/FiveInRowGame.cs
@@ -10,15 +10,65 @@ namespace FiveInRowDomain
     {
         public IList<Move> Moves { get; private set; } = new List<Move>();
         private Mover NextMover { get; set; } = Mover.X;
+        private Dictionary<(int x, int y), Mover> moveDict = new(); 
         public bool AddMove(int x, int y)
         {
-            var occupied = Moves.Where(m => (m.X == x) && (m.Y == y)).Any();
+            var occupied = moveDict.ContainsKey((x, y));//Moves.Where(m => (m.X == x) && (m.Y == y)).Any();
             if (occupied) return false;
-            var move = new Move() { X = x, Y = y, Mover = NextMover };
+            var mover = NextMover;
+            var move = new Move() { X = x, Y = y, Mover = mover };
             Moves.Add(move);
+            moveDict[(x, y)] = mover;
             NextMover = NextMover == Mover.X ? Mover.O : Mover.X;
             return true;
         }
+
+        public Mover? WhoWon()
+        {
+            foreach (var move in Moves)
+            {
+                if (CheckDirection(move, 1, 0) || // горизонталь
+                    CheckDirection(move, 0, 1) || // вертикаль
+                    CheckDirection(move, 1, 1) || // диагональ вниз
+                    CheckDirection(move, 1, -1))  // диагональ вверх
+                {
+                    return move.Mover;
+                }
+            }
+            return null;
+        }
+
+        private bool CheckDirection(Move startMove, int deltaX, int deltaY)
+        {
+            int count = 1;
+            int x = startMove.X;
+            int y = startMove.Y;
+
+            // Проверяем в одну сторону
+            count += CountInDirection(x, y, deltaX, deltaY, startMove.Mover);
+            // Проверяем в другую сторону
+            count += CountInDirection(x, y, -deltaX, -deltaY, startMove.Mover);
+
+            return count >= 5;
+        }
+
+        private int CountInDirection(int startX, int startY, int deltaX, int deltaY, Mover mover)
+        {
+            int count = 0;
+            int x = startX + deltaX;
+            int y = startY + deltaY;
+
+//            while (Moves.Any(m => m.X == x && m.Y == y && m.Mover == mover))
+            while (moveDict.ContainsKey((x, y)) && moveDict[(x, y)] == mover)
+            {
+                count++;
+                x += deltaX;
+                y += deltaY;
+            }
+
+            return count;
+        }
+
     }
 
     public class Move
