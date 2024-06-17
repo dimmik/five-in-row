671ea8fcdce04d63c4415b4430fe648c9f9c5084
dimmik <ad@dimmik.org>
2024-06-17 22:26:49 +0200

    check arch before connecting to hub

--- FiveInRow.Client/ClientMain.cs
+++ FiveInRow.Client/ClientMain.cs
@@ -2,10 +2,15 @@ using FiveInRow.Client;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+internal class ClientMain
+{
+    private static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-//builder.Services.AddAuthorizationCore();
-//builder.Services.AddCascadingAuthenticationState();
-//builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
-
-await builder.Build().RunAsync();
+        //builder.Services.AddAuthorizationCore();
+        //builder.Services.AddCascadingAuthenticationState();
+        //builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+        await builder.Build().RunAsync();
+    }
+}
